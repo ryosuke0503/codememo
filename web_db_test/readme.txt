6d459c0604d08c0c9a7de30e8cbf8f1ad98c396f
ryosuke0503 <ryosuke.m0503@gmail.com>
2021-04-07 19:07:44 +0900

    add all files

---
+++ web_db_test/readme.txt
@@ -0,0 +1,9 @@
+ファイルを保存して移動した後
+npm install
+npm start
+でサーバーが稼働します。
+http://localhost:3000/page1/info
+http://localhost:3000/page1/result
+にブラウザでアクセスするとくじ情報、くじ結果の(仮)ページよりtoto公式から引用した表が見れます。
+
+views/page1.ejsとviews/page2.ejsがそれぞれのページのひな型になっています。
---
+++ web_db_test/routes/page1.js
@@ -0,0 +1,39 @@
+　　　var express = require('express');
+　　　var router = express.Router();
+
+　　　var sqlite3 = require('sqlite3');   //データベースアクセス
+　　　var db = new sqlite3.Database('../../sqlite3/matchresult.sqlite3'); //データベースファイル
+
+
+　　　/* GET home page. */
+　　　router.get('/info', function(req, res, next) {
+
+　　　    //検索
+　　　    db.all('SELECT * FROM info',(err,rows)=>{
+　　　        if(!err){
+　　　            var data = { result:'success',message:'件検索されました。',recordList:rows};
+　　　            res.render('page1', data);
+　　　        } else {
+　　　            var data = { result:'err',message:'データベース接続に失敗しました。'};
+　　　            res.render('page1',data);
+　　　        }
+　　　    });
+    
+　　　  });
+
+　　　router.get('/result', function(req, res, next) {
+
+    　　　    //検索
+    　　　    db.all('SELECT * FROM result',(err,rows)=>{
+    　　　        if(!err){
+    　　　            var data = { result:'success',message:'件検索されました。',recordList:rows};
+    　　　            res.render('page2', data);
+    　　　        } else {
+    　　　            var data = { result:'err',message:'データベース接続に失敗しました。'};
+    　　　            res.render('page2',data);
+    　　　        }
+    　　　    });
+        
+    　　　  });
+
+　　　module.exports = router;
